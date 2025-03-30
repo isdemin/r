a9face559f49fba9115b80bcad0a9e90c5ba211b
isdemin <68160965+isdemin@users.noreply.github.com>
2025-03-30 15:57:41 +0300

    Add files via upload

---
+++ func.r
@@ -0,0 +1,17 @@
+#Log - запись в текстовый лог
+
+### Запись в текстовый лог
+Log=function(line1='', filename=paste0(gsub(':','-',Sys.time()),'.log') ){
+  append=(!file.exists(filename))
+  for (l in line1){
+    cat(l, file=filename, append=append)
+  }
+}
+
+a.number=19
+
+
+
+
+
+
